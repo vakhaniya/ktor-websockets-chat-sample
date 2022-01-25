d6f170452a0a32603a6f38348c3c3bcdef1fb1ce
vakhaniya <vakhaniya.ga@gmail.com>
2022-01-25 21:32:34 +0300

    chat implementation

---
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Connection.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.handson.chat.server
+
+import io.ktor.http.cio.websocket.*
+import java.util.concurrent.atomic.AtomicInteger
+
+class Connection(val session: DefaultWebSocketSession) {
+    companion object {
+        val lastId = AtomicInteger(0)
+    }
+
+    val name = "user${lastId.getAndIncrement()}"
+}
